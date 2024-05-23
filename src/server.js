797d114313c427ef4c9a5c18feccfc4e881c7f24
lastcocroach <yashkhemani100@gmail.com>
2024-05-23 13:29:39 +0530

    changed path of frontend

--- src/server.js
+++ src/server.js
@@ -26,10 +26,12 @@ app.use(express.json()); //to accept json data
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "src/frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname1, "src", "frontend", "build", "index.html")
+    );
   });
 } else {
   app.get("/", (req, res) => {
